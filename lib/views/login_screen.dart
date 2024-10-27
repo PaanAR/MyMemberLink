4f223a6288ede4f6d1532bdaf5f0fe5b75de4050
FarhanRashid <farhanrashid293@gmail.com>
2024-10-27 20:36:33 +0800

    loginPage part 2

--- lib/views/login_screen.dart
+++ lib/views/login_screen.dart
@@ -8,21 +8,39 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Column(children: [
-          const TextField(), // Add const to TextField
-          const TextField(), // Add const to TextField
-          Row(
-            children: const [
-              Text("Remember me"), // Add const to Text
-              Checkbox(value: true, onChanged: null), // Add const to Checkbox
-            ],
-          ),
-          MaterialButton(onPressed: onLogin, child: Text("Login"))
-        ]),
+        child: Padding(
+          padding: const EdgeInsets.all(32.0),
+          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+            TextField(
+              controller: emailController,
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(10)))),
+            ),
+            SizedBox(
+              height: 20,
+            ), // Add const to TextField
+            TextField(
+              controller: passwordController,
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(10)))),
+            ), // Add const to TextField
+            Row(
+              children: const [
+                Text("Remember me"), // Add const to Text
+                Checkbox(value: true, onChanged: null), // Add const to Checkbox
+              ],
+            ),
+            MaterialButton(onPressed: onLogin, child: const Text("Login"))
+          ]),
+        ),
       ),
     );
   }
